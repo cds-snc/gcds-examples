80cda900a1181612d21bb4979cd39f8bc757d541
Daine Trinidad <daine.trinidad@cds-snc.ca>
2024-05-23 16:02:49 -0700

    chore: updates; adds new gcds components vue package

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -16,71 +16,10 @@ const selectedColor = defineModel('selectedColor');
 </script>
 
 <template>
-  <ColorsVue />
-  <Header />
-  <HelloWorld msg="Hello, World!" />
-
-  <hr />
-
-  <div>Selected colour: {{ selectedColor }}</div>
-
-  <hr />
-  <h3>Using native select elements</h3>
-
   <div>
-    <label>Native select (events)</label>
-    <select
-      @change="selectedColor = $event.target.value"
-      :value="selectedColor"
-    >
-      <option
-        v-for="color in hairColors"
-        :key="color.id"
-        :text="color.descriptionEnglish"
-        :value="color.id"
-      ></option>
-    </select>
-  </div>
-  <div>
-    <label>Native select (v-model)</label>
-    <select v-model="selectedColor">
-      <option
-        v-for="color in hairColors"
-        :key="color.id"
-        :text="color.descriptionEnglish"
-        :value="color.id"
-      ></option>
-    </select>
-  </div>
-
-  <hr />
-  <h3>Using GCDS</h3>
-  <div>
-    <gcds-select
-      select-id="color-selector"
-      label="Select a colour (events)"
-      @change="
-        console.log($event.target);
-        selectedColor = $event.target.value;
-      "
-      :value="selectedColor"
-    >
-      <option v-for="color in hairColors" :value="color.id" :key="color.id">
-        {{ color.descriptionEnglish }}
-      </option>
-    </gcds-select>
-  </div>
-
-  <div>
-    <gcds-select
-      select-id="color-selector"
-      label="Select a colour (v-model)"
-      v-model="selectedColor"
-    >
-      <option v-for="color in hairColors" :value="color.id" :key="color.id">
-        {{ color.descriptionEnglish }}
-      </option>
-    </gcds-select>
+    <Header />
+    <HelloWorld msg="Hello, World!" />
+    <ColorsVue />
   </div>
 </template>
 
