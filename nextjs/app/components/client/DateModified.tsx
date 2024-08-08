cc7736f77c85eccfb3fe11042170b29d097f9bf3
Melanie Boeckmann <melanie.bockmann@gmail.com>
2024-08-08 10:02:08 -0700

    feat: add custom 404 page

--- nextjs/app/components/client/DateModified.tsx
+++ nextjs/app/components/client/DateModified.tsx
@@ -4,5 +4,5 @@ import { GcdsDateModified } from "@cdssnc/gcds-components-react-ssr";
 import { FC } from "react";
 
 export const DateModified: FC = () => (
-    <GcdsDateModified>2024-04-25</GcdsDateModified>
+    <GcdsDateModified>2024-08-06</GcdsDateModified>
 );
