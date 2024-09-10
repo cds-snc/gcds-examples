70c02de4cce88c17942bc984022f08b2722cf482
Ethan Wallace <ethan.wallace.91@gmail.com>
2024-09-10 18:05:08 -0400

    feat: holiday app about page (#26)
    * Empty commit for proper PR
    
    * feat: Add about page to fable test app (#22)
    
    * feat: Add about page to fable test app
    
    * PR feedback: Move images to public folder
    
    * Add internal versions of components

--- fable-test-app/src/pages/About.tsx
+++ fable-test-app/src/pages/About.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
 
 // Components (internal)
-import { DateModified, Heading } from '../components';
+import { DateModified, Heading, Text, Card, Grid } from '../components';
 
 const About: React.FC = () => {
   return (
     <section>
       <Heading tag="h1">About this app</Heading>
+
+      <Text>
+        This app was built using GC Design System styles and components.
+      </Text>
+
+      <Heading tag="h2">Learn more about the GC Design System</Heading>
+
+      <Grid
+        columnsTablet="1fr 1fr"
+      >
+        <Card
+          cardTitle="Figma library"
+          description="View all of our styles and components to use in your designs."
+          cardTitleTag="h3"
+          badge="Design"
+          href="https://www.figma.com/design/mh2maMG2NBtk41k1O1UGHV/GC-Design-System?node-id=4-1006&node-type=CANVAS&t=YFNAbrqORUhggvuC-0"
+          imgSrc="/figma.png"
+          imgAlt=""
+        />
+        <Card
+          cardTitle="Documentation site"
+          description="View all of our styles and components along with guidance on how to use them."
+          cardTitleTag="h3"
+          badge="Guidance"
+          href="https://design-system.alpha.canada.ca"
+          imgSrc="/docs.png"
+          imgAlt=""
+        />
+        <Card
+          cardTitle="GitHub repo"
+          description="View our code in Github for all of our components."
+          cardTitleTag="h3"
+          badge="Code"
+          href="https://github.com/cds-snc/gcds-components"
+          imgSrc="github.png"
+          imgAlt=""
+        />
+      </Grid>
+
+
+      <Heading tag="h2">Special thanks to Paul Craig</Heading>
+
+      <Text>
+        Paul Craig is the original creator of this site...
+      </Text>
 
       <DateModified>2024-08-28</DateModified>
     </section>
