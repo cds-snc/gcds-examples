ee1b8e6a4c989de07d45141aeb974ca96743e2d1
Daine Trinidad <daine.trinidad@cds-snc.ca>
2024-10-16 14:38:42 -0700

    feat: vue starter app (#21)
    * chore: add wip pieces
    
    * chore: add routing and i18n
    
    * chore: add routing, breadcrumbs, 404
    
    * add tests and updated bug report form
    
    * chore: add tests
    
    * chore: add intro text
    
    * chore: remove typescript - not in definition of done
    
    * chore: remove logo
    
    * chore: cleanup
    
    * chore: internal components + updated tests
    
    * chore: change p to text
    
    * chore: change favicon
    
    * fix: deps
    
    * chore: update playwright
    
    * fix: deps package lock
    
    * chore: update deps
    
    * docs: update readme
    
    * chore: PR feedback changes
    
    * chore: deps update, put back alignment=right on top nav
    
    * chore: PR feedback
    
    * chore: add text instead of lorem ipsum
    
    * chore: npm audit fix
    
    * chore: missed one file
    
    * chore: update package versions
    
    * chore: PR review changes
    
    * chore: update comment

---
+++ starter-apps/vue/vue-template/src/main.js
@@ -0,0 +1,20 @@
+import '@cdssnc/gcds-components-vue/gcds.css';
+import '@cdssnc/gcds-utility/dist/gcds-utility.min.css';
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { GcdsComponents } from '@cdssnc/gcds-components-vue';
+
+
+import App from './App.vue'
+import i18n from '@/i18n/index.js';
+import router from './router'
+
+// const app = createApp(App)
+const app = createApp(App);
+app.use(GcdsComponents)
+app.use(i18n.i18n)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
