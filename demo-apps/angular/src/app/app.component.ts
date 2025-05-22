fa6467181542c6b178055e79c75710f2757b1cba
Ethan Wallace <ethan.wallace.91@gmail.com>
2025-05-22 08:46:53 -0400

    Add examples of form components with ngModel

--- demo-apps/angular/src/app/app.component.ts
+++ demo-apps/angular/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GcdsComponentsModule } from '@cdssnc/gcds-components-angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GcdsComponentsModule],
+  imports: [RouterOutlet, GcdsComponentsModule, FormsModule],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-app';
+  name: string = '';
+  select: string = '';
+  date: string = '';
+  number: number = 0;
 }
