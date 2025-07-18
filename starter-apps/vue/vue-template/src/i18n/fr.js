132f42d31e9463666edb64b84c03dde6ad55a4cc
Lagacé, Mathieu <mathieu.lagace@dfo-mpo.gc.ca>
2025-07-18 10:55:12 -0400

    first translation draft

--- starter-apps/vue/vue-template/src/i18n/fr.js
+++ starter-apps/vue/vue-template/src/i18n/fr.js
@@ -4,20 +4,22 @@ export const fr = {
     heading: 'Accueil',
     paragraph:
       "Bienvenue dans l'application Vue Starter qui exploite les composants du système de conception GC !",
-    paragraph2: "Ce modèle d'application de démarrage est conçu pour aider les développeurs à configurer rapidement une application Vue moderne tout en adhérant aux normes d'accessibilité et de conception définies par le gouvernement du Canada.",
-    paragraph3: "You'll find all the necessary setup, including Vue Router, Pinia for state management, VITest for unit testing, and a selection of GCDS components to get you started on your project."
+    paragraph2:
+      "Ce modèle d'application de démarrage est conçu pour aider les développeurs à configurer rapidement une application Vue moderne tout en adhérant aux normes d'accessibilité et de conception définies par le gouvernement du Canada.",
+    paragraph3:
+      "You'll find all the necessary setup, including Vue Router, Pinia for state management, VITest for unit testing, and a selection of GCDS components to get you started on your project."
   },
   about: 'À propos',
   aboutTopic: 'Sujet',
   aboutPage: {
     heading: 'À propos',
     intro:
-      'Ce modèle de démarrage Vue intègre composants du Système de design GC, offrant une base solide pour la création d\'applications Web modernes et réactives. Il rationalise le développement en proposant des éléments d\'interface utilisateur prédéfinis et personnalisables qui garantissent la cohérence et la flexibilité de la conception. Parfait pour les projets à petite échelle comme pour les applications de grande envergure, ce modèle vous aide à vous concentrer sur la fourniture d\'une expérience utilisateur soignée avec une configuration minimale.',
+      "Ce modèle de démarrage Vue intègre composants du Système de design GC, offrant une base solide pour la création d'applications Web modernes et réactives. Il rationalise le développement en proposant des éléments d'interface utilisateur prédéfinis et personnalisables qui garantissent la cohérence et la flexibilité de la conception. Parfait pour les projets à petite échelle comme pour les applications de grande envergure, ce modèle vous aide à vous concentrer sur la fourniture d'une expérience utilisateur soignée avec une configuration minimale.",
     aboutTopic: {
       name: 'Sujet',
       heading: 'Sujet',
       intro:
-        'Il s\'agit d\'un exemple de page de rubrique au sein d\'une page À propos parente. Ici, vous pouvez voir comment la navigation par fil d\'Ariane se comporte au sein de la structure du site. Le fil d\'Ariane fournit un guide visuel pour l\'emplacement actuel sur le site, permettant une navigation transparente entre les sections.',
+        "Il s'agit d'un exemple de page de rubrique au sein d'une page À propos parente. Ici, vous pouvez voir comment la navigation par fil d'Ariane se comporte au sein de la structure du site. Le fil d'Ariane fournit un guide visuel pour l'emplacement actuel sur le site, permettant une navigation transparente entre les sections."
     }
   },
   french: 'Français',
@@ -27,8 +29,7 @@ export const fr = {
   homeNavLink: 'Application Vue',
   reportABugPage: {
     heading: 'Signaler un bug',
-    intro:
-      'Remplissez un rapport de bogue pour nous aider à améliorer Système de design GC.',
+    intro: 'Remplissez un rapport de bogue pour nous aider à améliorer Système de design GC.',
     form: {
       versionNumber: 'Paquet et version des composants de Système de design GC',
       versionNumberHint: "ex. {'@'}cdssnc/gcds-components-vue{'@'}0.x.x",
