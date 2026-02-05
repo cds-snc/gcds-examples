f882bb228b27b36e844ab6f886a42d084628add6
Melanie Boeckmann <melanie.bockmann@gmail.com>
2026-02-04 18:55:56 -0800

    chore(deps): update components to latest version + address breaking changes

--- fable-test-app/src/components/Card.tsx
+++ fable-test-app/src/components/Card.tsx
@@ -1,38 +1,42 @@
-import React from 'react';
+import React from "react";
 import { GcdsCard } from "@cdssnc/gcds-components-react";
 
 interface CardProps {
   children?: React.ReactNode;
   cardTitle: string;
-  cardTitleTag: "a" | "h3" | "h4" | "h5" | "h6";
+  cardTitleTag: "h3" | "h4" | "h5" | "h6";
   href: string;
   description?: string;
   badge?: string;
   imgSrc?: string;
-  imgAlt?: string
+  imgAlt?: string;
 }
 
-const Card: React.FC<CardProps> = React.memo(({
-  badge,
-  cardTitle,
-  cardTitleTag = "a",
-  description,
-  href,
-  imgSrc,
-  imgAlt,
-  children
-}) => (
-  <GcdsCard
-    badge={badge}
-    cardTitle={cardTitle}
-    cardTitleTag={cardTitleTag}
-    description={description}
-    href={href}
-    imgSrc={imgSrc}
-    imgAlt={imgAlt}
-  >
-    {children}
-  </GcdsCard>
-));
+const Card: React.FC<CardProps> = React.memo(
+  ({
+    badge,
+    cardTitle,
+    cardTitleTag,
+    description,
+    href,
+    imgSrc,
+    imgAlt,
+    children,
+    ...rest
+  }) => (
+    <GcdsCard
+      badge={badge}
+      cardTitle={cardTitle}
+      cardTitleTag={cardTitleTag}
+      description={description}
+      href={href}
+      imgSrc={imgSrc}
+      imgAlt={imgAlt}
+      {...rest}
+    >
+      {children}
+    </GcdsCard>
+  ),
+);
 
 export default Card;
